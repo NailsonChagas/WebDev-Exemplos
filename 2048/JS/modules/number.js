42813343bcdc8f9d4e62eea4c6f3f2a4440a14e8
NailsonChagas <chagasnailson@gmail.com>
2021-11-16 14:22:38 -0300

    2048

---
+++ 2048/JS/modules/number.js
@@ -0,0 +1,70 @@
+import grid from "./grid.js";
+
+const number = {
+    numbers: [],
+    getElements: function(){
+        const numberElements = document.getElementsByClassName('number');
+
+        for(let numberElement of numberElements){
+            this.numbers.push(numberElement);
+        }
+    },
+    spawn: function(){
+        const emptyCellIndex = grid.randomEmptyCellIndex();
+
+        if(emptyCellIndex === false){
+            return false;
+        }
+
+        const numberElement = document.createElement('div');
+        const numberValue = 2;
+
+        numberElement.innerText = numberValue;
+        numberElement.dataset.value = numberValue;
+        numberElement.classList.add('number');
+
+        numberElement.style.top = `${grid.cells[emptyCellIndex].top}px`;
+        numberElement.style.left = `${grid.cells[emptyCellIndex].left}px`;
+        //numberElement.style.backgroundColor = "#FFF"
+
+        grid.cells[emptyCellIndex].number = numberElement;
+
+        grid.gridElement.append(numberElement);
+
+        return true;
+    },
+    moveTo: function(fromCell, toCell){
+        const number = fromCell.number;
+
+        if(toCell.number === null){
+            number.style.top = `${toCell.top}px`;
+            number.style.left = `${toCell.left}px`;
+
+            toCell.number = number;
+            fromCell.number = null;
+        }
+        else if(number.dataset.value === toCell.number.dataset.value){
+            number.style.top = `${toCell.top}px`;
+            number.style.left = `${toCell.left}px`;
+            number.style.opacity = '0';
+
+            //remover elemento após a transição
+            setTimeout(()=>{
+                grid.gridElement.removeChild(number);
+            }, 500);
+
+            const newNumberValue = toCell.number.dataset.value * 2;
+            
+            pontuacao = document.getElementById('pontuacao');
+            let aux = parseInt(pontuacao.innerText);
+            pontuacao.innerText = (aux + newNumberValue)
+
+            toCell.number.dataset.value = newNumberValue;
+            toCell.number.innerText = newNumberValue;
+
+            fromCell.number = null;
+        }
+    }
+}
+
+export default number;
---
+++ 2048/JS/script.js
@@ -0,0 +1,26 @@
+import grid from './modules/grid.js';
+
+grid.init();
+
+document.addEventListener('keyup', function(e){
+    let direction = null;
+
+    if(e.key === 'ArrowUp' || e.key === 'w' || e.key === "W"){
+        direction = "UP";
+    }
+    else if(e.key === 'ArrowDown' || e.key === 's' || e.key === "S"){
+        direction = "DOWN";
+    }
+    else if(e.key === 'ArrowLeft' || e.key === 'a' || e.key === "A"){
+        direction = "LEFT";
+    }
+    else if(e.key === 'ArrowRight' || e.key === 'd' || e.key === "D"){
+        direction = "RIGHT";
+    }
+
+    if(direction !== null){
+        grid.slide(direction);
+    }
+
+    return false;
+});
